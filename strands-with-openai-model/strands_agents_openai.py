59f4171d9dca84fbb57f201bacd371d85316f0af
Krittaprot <krittaprot.t@gmail.com>
2025-11-10 20:37:57 +0700

    update on 2025-11-10 20:37:57

---
+++ strands-with-openai-model/strands_agents_openai.py
@@ -0,0 +1,54 @@
+from strands import Agent, tool
+from strands_tools import calculator # Import the calculator tool
+import argparse
+import json
+from strands.models.litellm import LiteLLMModel
+import os
+from bedrock_agentcore.runtime import BedrockAgentCoreApp
+import boto3
+
+app = BedrockAgentCoreApp()
+
+ssm = boto3.client('ssm', region_name='ap-southeast-1')
+
+parameter = ssm.get_parameter(
+    Name='/llm-provider/openrouter/api-key',
+    WithDecryption=True
+)
+
+OPENROUTER_API_KEY = parameter['Parameter']['Value']
+
+# Set environment for LiteLLM
+os.environ["OPENROUTER_API_KEY"] = OPENROUTER_API_KEY
+os.environ["OPENROUTER_API_BASE"] = "https://openrouter.ai/api/v1"
+
+# Create a custom tool 
+@tool
+def weather():
+    """ Get weather """ # Dummy implementation
+    return "sunny"
+
+model = "openrouter/qwen/qwen3-30b-a3b-instruct-2507"
+litellm_model = LiteLLMModel(
+    model_id=model, params={"max_tokens": 32000, "temperature": 0.3}
+)
+
+
+agent = Agent(
+    model=litellm_model,
+    tools=[calculator, weather],
+    system_prompt="You're a helpful assistant. You can do simple math calculation, and tell the weather."
+)
+
+
+@app.entrypoint
+def strands_agent_open_ai(payload):
+    """
+    Invoke the agent with a payload
+    """
+    user_input = payload.get("prompt")
+    response = agent(user_input)
+    return response.message['content'][0]['text']
+
+if __name__ == "__main__":
+    app.run()
